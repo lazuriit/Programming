41bc84ce35dc9c9046858070c7d479524130f642
𝙡𝙖𝙯𝙪𝙧𝙞𝙩 <126769113+lazuriit@users.noreply.github.com>
2025-12-18 14:34:46 +0700

    Update main.c

--- lab10_struct/main.c
+++ lab10_struct/main.c
@@ -2,21 +2,17 @@
 #include "point.h"
 
 int main() {
-    double x1, y1, z1;
-    double x2, y2, z2;
+    Point3D A, B;
 
     printf("Введите координаты точки A (x y z): ");
-    scanf("%lf %lf %lf", &x1, &y1, &z1);
+    scanf("%lf %lf %lf", &A.x, &A.y, &A.z);
 
     printf("Введите координаты точки B (x y z): ");
-    scanf("%lf %lf %lf", &x2, &y2, &z2);
-
-    Point A = initPoint(x1, y1, z1);
-    Point B = initPoint(x2, y2, z2);
+    scanf("%lf %lf %lf", &B.x, &B.y, &B.z);
 
     double d = distance(A, B);
 
-    printf("Расстояние между точками: %.3f\n", d);
+    printf("Расстояние между точками A и B: %.2lf\n", d);
 
     return 0;
 }
