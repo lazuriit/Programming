0b2564122ac29659214d637deb8673ab6081ba8a
𝙡𝙖𝙯𝙪𝙧𝙞𝙩 <126769113+lazuriit@users.noreply.github.com>
2026-02-27 20:03:20 +0700

    Create pcgames.h

---
+++ lab13_Make_CMake/lab_for_3/include/pcgames.h
@@ -0,0 +1,17 @@
+#ifndef _PCGAMES_H_
+#define _PCGAMES_H_
+
+#define N 100
+
+struct Game {
+    char title[40];
+    char genre[20];
+    int year;
+    int rating;
+};
+
+void fillGames(struct Game games[]);
+void printGames(struct Game games[]);
+void selectionSortByRating(struct Game games[]);
+
+#endif
