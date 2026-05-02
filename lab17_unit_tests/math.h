971c83b31774b86bb436d58501d118d20d9bea5f
𝙡𝙖𝙯𝙪𝙧𝙞𝙩 <126769113+lazuriit@users.noreply.github.com>
2026-05-02 12:43:01 +0700

    Create math.h

---
+++ lab17_unit_tests/math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+int sum(int a, int b);
+int difference(int a, int b);
+int multiplication(int a, int b);
+float real_division(int a, int b);
+int remainder_of_division(int a, int b);
+
+#endif
