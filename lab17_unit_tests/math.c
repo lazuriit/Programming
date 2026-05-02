5cdf38580ce462871f41249957128e6e37f043a5
𝙡𝙖𝙯𝙪𝙧𝙞𝙩 <126769113+lazuriit@users.noreply.github.com>
2026-05-02 12:42:41 +0700

    Create math.c

---
+++ lab17_unit_tests/math.c
@@ -0,0 +1,17 @@
+#include "math.h"
+
+int sum(int a, int b){
+    return a + b;
+}
+int difference(int a, int b){
+    return a - b;
+}
+int multiplication(int a, int b){
+    return a * b;
+}
+float real_division(int a, int b){
+    return (float)a / b;
+}
+int remainder_of_division(int a, int b){
+    return a % b;
+}
